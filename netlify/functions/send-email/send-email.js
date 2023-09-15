c7501823a30ce8ee1a0fd9c0baec5f65c862bec4
jazbogross <57679104+jazbogross@users.noreply.github.com>
2023-09-15 18:38:08 +0200

    email function

--- netlify/functions/send-email/send-email.js
+++ netlify/functions/send-email/send-email.js
@@ -1,69 +1,73 @@
-// with thanks to https://github.com/Urigo/graphql-modules/blob/8cb2fd7d9938a856f83e4eee2081384533771904/website/lambda/contact.js
-const process = require('process')
-const { promisify } = require('util')
-const data = require('./static/words.json');
+const process = require('process');
+const { promisify } = require('util');
+const fetch = require('node-fetch');
+const sendMailLib = require('sendmail');
+const { validateEmail, validateLength } = require('./validations.js');
 
-const sendMailLib = require('sendmail')
+const sendMail = promisify(sendMailLib());
 
-const { validateEmail, validateLength } = require('./validations.js')
-
-const sendMail = promisify(sendMailLib())
-
-const NAME_MIN_LENGTH = 3
-const NAME_MAX_LENGTH = 50
-const DETAILS_MIN_LENGTH = 10
-const DETAILS_MAX_LENGTH = 1e3
+const NAME_MIN_LENGTH = 3;
+const NAME_MAX_LENGTH = 50;
+const DETAILS_MIN_LENGTH = 10;
+const DETAILS_MAX_LENGTH = 1e3;
 
 const handler = async (event) => {
   if (!process.env.CONTACT_EMAIL) {
     return {
       statusCode: 500,
       body: 'process.env.CONTACT_EMAIL must be defined',
-    }
+    };
   }
 
-  const body = JSON.parse(event.body)
+  const body = JSON.parse(event.body);
 
+  // Fetching data from the JSON file
+  const response = await fetch('https://soft-crostata-20d468.netlify.app/words.json');
+  const data = await response.json();
+
+  // Validation logic
   try {
-    validateLength('body.name', body.name, NAME_MIN_LENGTH, NAME_MAX_LENGTH)
+    validateLength('body.name', body.name, NAME_MIN_LENGTH, NAME_MAX_LENGTH);
   } catch (error) {
     return {
       statusCode: 403,
       body: error.message,
-    }
+    };
   }
 
   try {
-    validateEmail('body.email', body.email)
+    validateEmail('body.email', body.email);
   } catch (error) {
     return {
       statusCode: 403,
       body: error.message,
-    }
+    };
   }
 
   try {
-    validateLength('body.details', body.details, DETAILS_MIN_LENGTH, DETAILS_MAX_LENGTH)
+    validateLength('body.details', body.details, DETAILS_MIN_LENGTH, DETAILS_MAX_LENGTH);
   } catch (error) {
     return {
       statusCode: 403,
       body: error.message,
-    }
+    };
   }
 
+  // Prepare email descriptor
   const descriptor = {
     from: `"Automated Email" <no-reply@gql-modules.com>`,
     to: process.env.CONTACT_EMAIL,
-    subject: `Word Update`,
+    subject: 'Word Update',
     text: `Here is your daily info: ${JSON.stringify(data)}`,
-  }
+  };
 
+  // Send email
   try {
-    await sendMail(descriptor)
-    return { statusCode: 200, body: '' }
+    await sendMail(descriptor);
+    return { statusCode: 200, body: '' };
   } catch (error) {
-    return { statusCode: 500, body: error.message }
+    return { statusCode: 500, body: error.message };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
