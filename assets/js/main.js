a3883696e611d7bdcdb28758af1c3832096fad66
jazbogross <57679104+jazbogross@users.noreply.github.com>
2023-10-12 14:10:38 +0100

    shuffle words

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,17 @@ async function getSvgs() {
   }
 }
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+      [array[i], array[j]] = [array[j], array[i]]; // swap elements
+  }
+  return array;
+}
+
+
 async function populateContainer(data, container) {
+  data = shuffle(data);
   let soundFilenames = [];
   getSounds().then(filenames => {
     soundFilenames = filenames;
